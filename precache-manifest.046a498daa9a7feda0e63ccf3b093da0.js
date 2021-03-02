0e1d2335fa389716185d57f324e3719ddd9ec524
Fannypc <mars.poc@gmail.com>
2021-03-01 23:25:29 -0600

    Updates

---
+++ precache-manifest.046a498daa9a7feda0e63ccf3b093da0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292082fe3e1c54479e377d40d282025f",
+    "url": "/project-resume/index.html"
+  },
+  {
+    "revision": "5cecc8620ddfbae8557e",
+    "url": "/project-resume/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a684fb1ceaf0c465fbb3",
+    "url": "/project-resume/static/css/main.3602f122.chunk.css"
+  },
+  {
+    "revision": "5cecc8620ddfbae8557e",
+    "url": "/project-resume/static/js/2.b9372cb5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project-resume/static/js/2.b9372cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a684fb1ceaf0c465fbb3",
+    "url": "/project-resume/static/js/main.738cee2c.chunk.js"
+  },
+  {
+    "revision": "f08f762b0c4623ba2c8d",
+    "url": "/project-resume/static/js/runtime-main.b91122a4.js"
+  }
+]);
